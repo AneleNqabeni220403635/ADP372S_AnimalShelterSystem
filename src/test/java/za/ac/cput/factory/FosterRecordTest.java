02d1c4573f4b2caa52285581fc68517f17328612
222037881 <222037881@mycput.ac.za>
2024-05-18 01:18:30 +0200

    222037781

--- src/test/java/za/ac/cput/factory/FosterRecordTest.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.factory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.domain.FosterRecord;
-
-    interface FosterRecordRepository extends JpaRepository<FosterRecord, String> {
-
-    }
-
-
