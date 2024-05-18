1addc5c3644dca6c83a13726c10a33a49361206e
HNeet <220526303@mycput.ac.za>
2024-05-18 18:04:46 +0200

    Remove extra stuff

--- src/main/java/za/ac/cput/repository/IncidentReportRepository.java
+++ src/main/java/za/ac/cput/repository/IncidentReportRepository.java
@@ -1,9 +1,10 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.IncidentReport;
 
 @Repository
-public class IncidentReportRepository //extends IIncidentReportRepository
+public interface IncidentReportRepository extends JpaRepository<IncidentReport, Long>
 {
 }
-
--- src/main/java/za/ac/cput/repository/LostAndFoundRepository.java
+++ src/main/java/za/ac/cput/repository/LostAndFoundRepository.java
@@ -1,8 +1,10 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.LostAndFound;
 
 @Repository
-public class LostAndFoundRepository // extends ILostAndFoundRepository
+public interface LostAndFoundRepository extends JpaRepository<LostAndFound, Long>
 {
 }
