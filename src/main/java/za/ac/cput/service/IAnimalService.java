eab3f9516af2db728d56d0d678efc43153f27d17
AneleNqabeni220403635 <220403635@mycput.ac.za>
2024-05-24 17:27:55 +0200

    Implemented a find all animals by name and age method. And additionally did some tweaking.

--- src/main/java/za/ac/cput/service/IAnimalService.java
+++ src/main/java/za/ac/cput/service/IAnimalService.java
@@ -6,5 +6,5 @@ import java.util.Set;
 
 public interface IAnimalService extends IService<Animal, Long>{
     Set <Animal> getall();
-
+    Set<Animal> findAllAnimalsByNameAndAge(String name, int age);
 }
