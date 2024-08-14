245ca9798025ded3fcca655352b495531f49c8df
LENOVO <220403635@mycput.ac.za>
2024-08-14 10:39:01 +0200

    Renamed animal factory to dog factory then tweaked the helper class

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,44 +1,24 @@
 package za.ac.cput.util;
 
-import java.time.LocalDate;
-import java.util.concurrent.atomic.AtomicLong;
-
 public class Helper {
-    private static final AtomicLong counter = new AtomicLong(1000);
 
-    public static boolean isNullorEmpty(String s) {
-        return s == null || s.isEmpty();
+    public static boolean isNullorEmpty(String str) {
+        return str == null || str.isEmpty();
     }
 
-    public static boolean isNullorZero(Long l) {
-        return l == null || l == 0;
+    public static boolean isLessThanOrEqualToZero(int number) {
+        return number <= 0;
     }
 
-    public static boolean isLessThanOrEqualToZero(int num) {
-        return num <= 0;
+    public static boolean isNullorZero(long number) {
+        return number == 0;
     }
 
-    public static boolean isValidAnimal(Long animal) {
-        return animal != null && animal > 0;
+    public static long generateDogId() {
+        return System.currentTimeMillis();
     }
 
-    public static boolean isValidLocalDateVaccinationDate(LocalDate date) {
-        return date != null && date.isAfter(LocalDate.now());
-    }
-
-    public static boolean isValidLocalDateNextCheckUp(LocalDate date) {
-        return date != null && date.isAfter(LocalDate.now());
-    }
-
-    public static boolean isValidMedication(String medication) {
-        return !isNullorEmpty(medication);
-    }
-
-    public static boolean isValidBehaviour(String behaviour) {
-        return !isNullorEmpty(behaviour);
-    }
-
-    public static Long generateAnimalCode() {
-        return counter.incrementAndGet();
+    public static long generateMedicalRecordId() {
+        return System.currentTimeMillis();
     }
 }
