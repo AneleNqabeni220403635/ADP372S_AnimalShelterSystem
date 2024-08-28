a47d230f31e99628feb3fdd365bbd564d3663e1a
222037881 <222037881@mycput.ac.za>
2024-08-28 22:30:44 +0200

    222037881

--- src/main/java/za/ac/cput/service/Impl/IOwnerRecordService.java
+++ src/main/java/za/ac/cput/service/Impl/IOwnerRecordService.java
@@ -1,9 +1,11 @@
 package za.ac.cput.service.Impl;
 
+import org.springframework.stereotype.Service;
 import za.ac.cput.domain.OwnerRecord;
 
 import java.util.Set;
 
+@Service
 public interface IOwnerRecordService extends IService<OwnerRecord,Long> {
     Set<OwnerRecord> getall();
 }
--- src/main/java/za/ac/cput/service/Impl/IPetOwnerService.java
+++ src/main/java/za/ac/cput/service/Impl/IPetOwnerService.java
@@ -1,9 +1,11 @@
 package za.ac.cput.service.Impl;
 
+import org.springframework.stereotype.Service;
 import za.ac.cput.domain.PetOwner;
 
 import java.util.Set;
 
+@Service
 public interface  IPetOwnerService extends IService<PetOwner,Long> {
     Set<PetOwner> getall();
 }
